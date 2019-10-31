c8125153076c2e4ab807d71b16ecb7f03dbc12d8
malduke29 <43395103+malduke29@users.noreply.github.com>
2019-10-31 15:54:04 -0300

    Update Node->Php.js

--- Final/Node->Php.js
+++ Final/Node->Php.js
@@ -1,11 +1,13 @@
-var data = {
+var data = {                    //se define la info que se quiere pasar, aqui seria el JWT o el  JWT decodificado
    var1:"something", 
    var2:"something else"
 };
+
+
 var querystring = require("querystring");
 var qs = querystring.stringify(data);
 var qslength = qs.length;
-var options = {
+var options = {            //aqui vendria las datos de donde quieres enviar la info
     hostname: "example.com",
     port: 80,
     path: "some.php",
@@ -16,9 +18,9 @@ var options = {
     }
 };
 
-var buffer = "";
-var req = http.request(options, function(res) {
-    res.on('data', function (chunk) {
+var buffer = ""; //se define la varibale que se enviara
+var req = http.request(options, function(res) {     //se realiza una request con los datos especificados anteriormente y la siguiente funcion
+    res.on('data', function (chunk) {      //la funcion toma la info de la variable data y la coloca en buffer
        buffer+=chunk;
     });
     res.on('end', function() {
